3f77e19bf6d6610db8d9739af442c2f8ca97ed0f
Johannes Förstner <johannes@famfoerstner.de>
2023-12-27 17:05:52 +0100

    Get GasPy involved for pre-build checks

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 :: This script is supposed to be executed from your DS installation folder.
-:: TankCreator is expected to be in a sibling dir.
+:: TankCreator and gaspy are expected to be in sibling dirs.
 
 :: map name
 set map=ehland
@@ -11,13 +11,47 @@ set doc_ds=%USERPROFILE%\Documents\Dungeon Siege
 set ds=.
 :: path of TankCreator
 set tc=..\TankCreator
+:: path of GasPy
+set gaspy=..\gaspy
 
 set copyright=CC-BY-SA 2023
 set author=Johannes Förstner
 
+:: param
+set mode=%1
+echo %mode%
+
+:: pre-build checks
+pushd %gaspy%
+setlocal EnableDelayedExpansion
+if not "%mode%"=="light" (
+  venv\Scripts\python -m build.check_player_world_locations %map% --bits DS1
+  if !errorlevel! neq 0 pause
+  venv\Scripts\python -m build.check_lore %map% --bits DS1
+  if !errorlevel! neq 0 pause
+  venv\Scripts\python -m build.check_moods %map% --bits DS1
+  if !errorlevel! neq 0 pause
+  venv\Scripts\python -m build.check_quests %map% --bits DS1
+  if !errorlevel! neq 0 pause
+  venv\Scripts\python -m build.check_dupe_node_ids %map% --bits DS1
+  if !errorlevel! neq 0 pause
+  venv\Scripts\python -m build.check_tips %map% --bits DS1
+  if !errorlevel! neq 0 pause
+)
+if "%mode%"=="release" (
+  venv\Scripts\python -m build.check_cam_blocks %map% --bits DS1
+  if !errorlevel! neq 0 pause
+)
+endlocal
+popd
+
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_ds%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+pushd %gaspy%
+venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
+if %errorlevel% neq 0 pause
+popd
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
